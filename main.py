6b131d7f1dbc28cb0f83d88e702489ca3e9c993c
Kingofhollows099 <kingofhollows099@gmail.com>
2023-06-15 12:30:28 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import pygame as pg
 import settings as s
 
+
 pg.init()
 screen = pg.display.set_mode((s.SCREEN_WIDTH, s.SCREEN_HEIGHT))
 clock = pg.time.Clock()
@@ -11,13 +12,17 @@ while running:
         if event.type == pg.QUIT:
             running = False
     
+    screen.fill("black")
 
-    screen.fill("purple")
+#Game Code Here:
+    keys = pg.key.get_pressed()
+    def pressed(key):
+        return keys[pg.K_ + key]
 
-
-
+    if keys[pg.K_w] :
+        pg.player_pos.y -= 300 * dt
     pg.display.flip()
 
     clock.tick(s.FPS)
-
+    dt = clock.tick(60) / 1000
 pg.quit()
