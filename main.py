94827e08d375d44b2c4cd22ff7ce5d28800d75e1
BIGBOBBLE123 <108598547+BIGBOBBLE123@users.noreply.github.com>
2023-06-15 10:28:22 -0500

    asdas

--- main.py
+++ main.py
@@ -1,2 +1,23 @@
 import pygame as pg
+import settings as Settings
 
+pg.init()
+screen = pg.display.set_mode((Settings.SCREEN_WIDTH, Settings.SCREEN_HEIGHT))
+clock = pg.time.Clock()
+running = True
+
+while running:
+    for event in pg.event.get():
+        if event.type == pg.QUIT:
+            running = False
+
+
+    screen.fill("purple")
+
+
+
+    pg.display.flip()
+
+    clock.tick(Settings.FPS)
+
+pg.quit()
