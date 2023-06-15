d70ba02ca0fb93177b321beacbfc0615fcbf4e76
BIGBOBBLE123 <108598547+BIGBOBBLE123@users.noreply.github.com>
2023-06-15 12:49:25 -0500

    asd

--- main.py
+++ main.py
@@ -1,23 +1,37 @@
 import pygame as pg
+import random
 import settings as s
 
-pg.init()
-screen = pg.display.set_mode((s.SCREEN_WIDTH, s.SCREEN_HEIGHT))
-clock = pg.time.Clock()
-running = True
-
-while running:
-    for event in pg.event.get():
-        if event.type == pg.QUIT:
-            running = False
+class Program():
     
-
-    screen.fill("purple")
-
-
-
-    pg.display.flip()
-
-    clock.tick(s.FPS)
-
-pg.quit()
+    def __init__(self):
+        pg.init()
+        self.screen = pg.display.set_mode((s.SCREEN_WIDTH, s.SCREEN_HEIGHT))
+        self.clock = pg.time.Clock()
+        self.background_surf = pg.Surface((s.SCREEN_WIDTH, s.SCREEN_HEIGHT))
+        
+        
+        self.running = True
+        self.gameloop(self.running)
+        
+    def update(self):
+        self.screen.fill("purple")
+        self.background_surf.fill(s.BACKGROUND_COLOR)
+        pg.Surface.blit(self.screen, self.background_surf, (0, 0))
+        
+        pg.display.flip()
+        self.clock.tick(s.FPS)
+        
+            
+    def gameloop(self, running):
+        while running:
+            for event in pg.event.get():
+                if event.type == pg.QUIT:
+                    running = False
+                if event.type == pg.MOUSEBUTTONDOWN:
+                    print('surface')
+                    
+            self.update()
+        pg.quit()
+            
+Program()
